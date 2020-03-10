f1551130801fb399d18cd2240dcce487a49d2e11
Arithus S <arithus@netkasystem.com>
2020-03-10 14:26:53 +0700

    - Add NotificationHelpers

---
+++ Backend/NetkaCommitment.Common/NotificationHelpers.cs
@@ -0,0 +1,42 @@
+﻿using FirebaseAdmin.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetkaCommitment.Common
+{
+    public static class NotificationHelpers
+    {
+        public static async Task<List<SendResponse>> SendPushNotificationAsync(List<string> lsRegistationToken) {
+            var message = new MulticastMessage()
+            {
+                Tokens = lsRegistationToken,
+                Data = new Dictionary<string, string>()
+                {
+                    { "score", "850" },
+                    { "time", "2:45" },
+                },
+            };
+
+            var response = await FirebaseMessaging.DefaultInstance.SendMulticastAsync(message);
+           return response.Responses.ToList();
+        }
+
+        public static async Task<string> SendPushNotificationAsync(string registationToken)
+        {
+            var message = new Message()
+            {
+                Token = registationToken,
+                Data = new Dictionary<string, string>()
+                {
+                    { "score", "850" },
+                    { "time", "2:45" },
+                },
+            };
+
+            return await FirebaseMessaging.DefaultInstance.SendAsync(message);
+        }
+    }
+}
